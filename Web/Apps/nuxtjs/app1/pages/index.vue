9f8d967aece669a647b5590fdda8afec39c6d549
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2021-08-15 16:51:26 +0300

    learn nuxt

---
+++ Web/Apps/nuxtjs/app1/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <Header />
+    <nuxt-link to="/users">Users</nuxt-link>
+    
+    <a href="/users">Users redirect</a>
+  </div>
+</template>
+
+<script>
+</script>
+
---
+++ Web/Apps/nuxtjs/app2/components/Posts/PostList.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="post-list">
+      <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://i.pinimg.com/originals/74/22/87/742287c1114542dd95e2befa3e653c06.png"
+      title="Hello there"
+      previewText = "This is my first post!" />
+      <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://i.pinimg.com/originals/74/22/87/742287c1114542dd95e2befa3e653c06.png"
+      title="Hello there"
+      previewText = "This is my post no!" />
+      <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://i.pinimg.com/originals/74/22/87/742287c1114542dd95e2befa3e653c06.png"
+      title="Hello there"
+      previewText = "This is my 3 post!" />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+    components: {
+        PostPreview
+    },
+    props: {
+      isAdmin: {
+        type: Boolean,
+        default: false
+      }
+    },    
+}
+</script>
+
+<style scoped>
+
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
