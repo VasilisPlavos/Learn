6e2acf941535953d9bb763611949976910f5010d
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2021-10-10 14:31:39 +0300

    9.11

--- Web/Apps/nodejs/homework1/app7.express.ejs.mvc/routes/admin.js
+++ Web/Apps/nodejs/homework1/app7.express.ejs.mvc/routes/admin.js
@@ -6,13 +6,17 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
-
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
+
+// /admin/edit-product => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 
 module.exports = router;
