ea353d156d0a82782e055e8c6666d3b07a384ebe
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2021-10-10 15:15:56 +0300

    Update app.js

--- Web/Apps/nodejs/homework1/app7.express.ejs.mvc/app.js
+++ Web/Apps/nodejs/homework1/app7.express.ejs.mvc/app.js
@@ -1,8 +1,5 @@
 const path = require('path');
-
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -13,7 +10,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
