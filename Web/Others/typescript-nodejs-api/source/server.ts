faf2a62741e798cca7a7c1ff32701f5557935394
Vasilis Plavos <vplavos@gmail.com>
2022-03-16 15:58:06 +0200

    move to folder

--- Web/Others/typescript-nodejs-api/source/server.ts
+++ Web/Others/typescript-nodejs-api/source/server.ts
@@ -21,15 +21,18 @@ router.use((req, res, next) => {
   next();
 });
 
+
 router.use("/", routes);
 
 router.use((req, res, next) => {
-  const error = new Error("not found");
+  const error = new Error("not found. try http://localhost:6060/posts");
   return res.status(404).json({ message: error.message });
 });
 
 const httpServer = http.createServer(router);
 const PORT: any = process.env.PORT ?? 6060;
-httpServer.listen(PORT, () =>
-  console.log(`server running on http://localhost:${PORT}`)
-);
+httpServer.listen(PORT, () => 
+{
+  console.log(`server running on http://localhost:${PORT}`);
+  console.log("source: https://www.section.io/engineering-education/how-to-create-a-simple-rest-api-using-typescript-and-nodejs/");
+});
