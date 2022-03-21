2e5e831a07af9ee6a31c40403c582ebb533cec5e
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-03-21 21:39:26 +0200

    bp

---
+++ dotnet/Examples/Example.Cloudon/Example.Cloudon.Web/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Example.Cloudon.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
