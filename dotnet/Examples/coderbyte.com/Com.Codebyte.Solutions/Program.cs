aa161f3cbf9695fe9bc8aece851a9af9210553ad
Vasilis Plavos <vplavos@gmail.com>
2022-03-30 12:28:05 +0300

    codebyte code

---
+++ dotnet/Examples/coderbyte.com/Com.Codebyte.Solutions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.Codebyte.Solutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
