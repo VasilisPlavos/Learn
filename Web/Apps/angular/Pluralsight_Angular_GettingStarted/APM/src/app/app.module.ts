40dccb253f98bb63d502dc4446dad5c6edb70ee0
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-06-02 17:36:04 +0300

    Add ProductList component

--- Web/Apps/angular/Pluralsight_Angular_GettingStarted/APM/src/app/app.module.ts
+++ Web/Apps/angular/Pluralsight_Angular_GettingStarted/APM/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
+  declarations: [AppComponent, ProductListComponent],
   imports: [BrowserModule],
-  declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
