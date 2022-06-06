6df747022857c06633eb0f57004702cf6c4e7d39
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-06-06 10:02:29 +0300

    nested component with param

--- Web/Apps/angular/Pluralsight_Angular_GettingStarted/APM/src/app/app.module.ts
+++ Web/Apps/angular/Pluralsight_Angular_GettingStarted/APM/src/app/app.module.ts
@@ -5,13 +5,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
-    AppComponent, 
+    AppComponent,
+    ConvertToSpacesPipe,
     ProductListComponent,
-    ConvertToSpacesPipe
+    StarComponent
   ],
+
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
