331d8676e6eaab2c4b57f88d558fd92743e9cbb0
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-06-06 13:19:04 +0300

    Update app.module.ts

--- Web/Apps/angular/Pluralsight_Angular_GettingStarted/APM/src/app/app.module.ts
+++ Web/Apps/angular/Pluralsight_Angular_GettingStarted/APM/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ProductDetailComponent } from './products/product-detail.component';
+import { StarComponent } from './shared/star.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ConvertToSpacesPipe,
+    ProductDetailComponent,
     ProductListComponent,
-    StarComponent,
-    ProductDetailComponent
+    StarComponent
   ],
 
   imports: [
