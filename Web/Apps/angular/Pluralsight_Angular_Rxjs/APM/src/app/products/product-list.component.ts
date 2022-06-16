5a64de6c596a3fc1e72862329845bd15670ae271
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-06-16 20:28:05 +0300

    sort by categories quick and dirty

--- Web/Apps/angular/Pluralsight_Angular_Rxjs/APM/src/app/products/product-list.component.ts
+++ Web/Apps/angular/Pluralsight_Angular_Rxjs/APM/src/app/products/product-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { catchError, EMPTY, Observable } from 'rxjs';
+import { catchError, EMPTY, map, Observable } from 'rxjs';
 
 import { Product } from './product';
 import { ProductCategory } from '../product-categories/product-category';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 import { ProductService } from './product.service';
 
 @Component({
@@ -15,6 +16,8 @@ export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
+  selectedCategoryId : number | undefined;
+  constructor(private productCategoryService: ProductCategoryService, private productService: ProductService) { }
 
   products$ : Observable<Product[]> = this.productService.productsWithCategory$
   .pipe(catchError(err => 
@@ -24,13 +27,34 @@ export class ProductListComponent {
       return EMPTY;
     }));
 
-  constructor(private productService: ProductService) { }
+    productsSimpleFilter$ = this.productService.productsWithCategory$
+      .pipe(map(products => 
+        products.filter(product => 
+          this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true)));
+
+    categories$ : Observable<ProductCategory[]> = this.productCategoryService.productCategories$
+      .pipe(catchError(err => {
+        console.log(err);
+        this.errorMessage = err; 
+        return EMPTY;
+      }));
 
   onAdd(): void {
     console.log('Not yet implemented');
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.selectedCategoryId = +categoryId;
+    this.productsSimpleFilter$ = this.productService.productsWithCategory$
+    .pipe(map(products => 
+      products.filter(product => 
+        this.selectedCategoryId ? product.categoryId === this.selectedCategoryId : true)));
+
+  this.categories$ = this.productCategoryService.productCategories$
+    .pipe(catchError(err => {
+      console.log(err);
+      this.errorMessage = err; 
+      return EMPTY;
+    }));
   }
 }
