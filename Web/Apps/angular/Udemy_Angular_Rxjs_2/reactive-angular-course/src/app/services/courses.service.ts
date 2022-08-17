955400a3626c6951738960af9579f0df12851ee7
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-08-17 22:08:13 +0300

    Update courses.service.ts

--- Web/Apps/angular/Udemy_Angular_Rxjs_2/reactive-angular-course/src/app/services/courses.service.ts
+++ Web/Apps/angular/Udemy_Angular_Rxjs_2/reactive-angular-course/src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 import { Course, sortCoursesBySeqNo } from "../model/course";
 
 @Injectable({ providedIn: 'root' })
@@ -12,6 +12,9 @@ export class CoursesService {
 
     loadAllCourses() : Observable<Course[]> {
         return this.http.get<Course[]>('/api/courses')
-        .pipe(map(res => res["payload"].sort(sortCoursesBySeqNo)));
+        .pipe(
+            map(res => res["payload"].sort(sortCoursesBySeqNo)),
+            shareReplay()
+            );
     }
 }
