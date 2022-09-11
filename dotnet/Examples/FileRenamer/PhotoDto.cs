73760033386e1e8f376cb08c286b3b979909f8dc
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-09-11 21:58:01 +0300

    codility test separate

---
+++ dotnet/Examples/FileRenamer/PhotoDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FileRenamer;
+
+public class PhotoDto
+{
+    public int ViewOrder { get; set; }
+    public string FileExt { get; set; }
+    public string Location { get; set; }
+    public DateTime DateCreated { get; set; }
+    public string NewFileName { get; set; }
+}
