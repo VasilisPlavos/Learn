a3dca40c3407ef9d249c18d85e4a62047eebb3e0
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-10-13 07:59:12 +0300

    Create app-express.middlewares.2.1.3.mjs

---
+++ Web/Apps/nodejs/mathesis/app-express.middlewares.2.1.3.mjs
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express();
+
+const myMw1 = (req, res, next) => {
+  console.log("myMw1");
+  res.send('ok');
+  next();
+};
+
+const myMw2 = (req, res, next) => {
+  console.log("myMw2");
+  next();
+};
+
+const myMw3 = (req, res, next) => {
+  console.log("myMw3");
+  next();
+};
+
+// app.use(myMw1, myMw2, myMw3);
+// app.use(myMw2);
+// app.use(myMw3);
+
+const pipeline = [ myMw1, myMw2, myMw3 ]
+app.use(pipeline);
+
+app.listen(3000, () => console.log(`app start at http://localhost:3000`));
