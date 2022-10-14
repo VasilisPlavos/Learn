41106dbb23b9b607f609ee65671de621eeb5b7e3
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-10-14 20:34:19 +0300

    Update app-express.middlewares.2.1.3.mjs

--- Web/Apps/nodejs/mathesis/express-middlewares-example/app-express.middlewares.2.1.3.mjs
+++ Web/Apps/nodejs/mathesis/express-middlewares-example/app-express.middlewares.2.1.3.mjs
@@ -4,16 +4,18 @@ const app = express();
 
 const myMw1 = (req, res, next) => {
   console.log("myMw1");
-  res.send('ok');
+  // res.send("ok1");
   next();
 };
 
 const myMw2 = (req, res, next) => {
   console.log("myMw2");
+  res.send("ok2");
   next();
 };
 
 const myMw3 = (req, res, next) => {
+  res.send("ok3");
   console.log("myMw3");
   next();
 };
@@ -22,8 +24,13 @@ const myMw3 = (req, res, next) => {
 // app.use(myMw2);
 // app.use(myMw3);
 
-const pipeline = [ myMw1, myMw3, myMw2 ]
+const pipeline = [myMw1, myMw3, myMw2];
 // app.use(pipeline);
-app.get('/', pipeline);
+// app.get('/', pipeline);
+// app.get('/', myMw1, myMw3, myMw2);
+
+app.get("/3", myMw3);
+app.get("/", myMw2,myMw1);
+app.use(myMw1);
 
 app.listen(3000, () => console.log(`app start at http://localhost:3000`));
