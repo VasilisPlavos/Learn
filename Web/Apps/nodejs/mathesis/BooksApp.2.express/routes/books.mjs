d049a99360733b486c0fccdb5e5f80fb09e36f9c
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-10-15 06:08:01 +0300

    manage express-validator

--- Web/Apps/nodejs/mathesis/BooksApp.2.express/routes/books.mjs
+++ Web/Apps/nodejs/mathesis/BooksApp.2.express/routes/books.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import { BookList } from "../bookList.mjs";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 const bookList = new BookList();
@@ -11,13 +12,26 @@ router.get("/books", async (req, res) => {
 
 router.get("/books/add", (req, res) => res.render("addbookform"));
 
-router.post("/books", async (req, res) => {
-  const book = {
-    title: req.body["title"],
-    author: req.body["author"],
-  };
-  await bookList.addBookToFileAsync(book);
-  res.redirect("/books");
-});
+router.post(
+  "/books",
+  body("title")
+  .isAlpha().trim().withMessage("chars only")
+  .isLength({ min: 5 }).withMessage("at least 5 chars"),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (errors.errors?.length > 0) {
+      var error = errors.mapped();
+      res.render("addbookform", { error: error });
+      return;
+    }
+
+    const book = {
+      title: req.body["title"],
+      author: req.body["author"],
+    };
+    await bookList.addBookToFileAsync(book);
+    res.redirect("/books");
+  }
+);
 
 export { router };
