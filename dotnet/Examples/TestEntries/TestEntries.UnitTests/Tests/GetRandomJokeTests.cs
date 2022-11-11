335d96bdef981818fcb389e72aa73ea650d7e94d
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-11-11 18:08:46 +0200

    Update GetRandomJokeTests.cs

--- dotnet/Examples/TestEntries/TestEntries.UnitTests/Tests/GetRandomJokeTests.cs
+++ dotnet/Examples/TestEntries/TestEntries.UnitTests/Tests/GetRandomJokeTests.cs
@@ -1,13 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 using Microsoft.AspNetCore.Mvc;
 using TestEntries.Api.Controllers;
-using TestEntries.Common.Contracts;
 using TestEntries.Common.Services;
 using TestEntries.Contracts.Contracts.Joke;
 
@@ -15,23 +8,24 @@ namespace TestEntries.UnitTests.Tests
 {
     public class GetRandomJokeTests
     {
-        private static readonly ContractAssert assert = new();
+        private static readonly ContractAssert Assert = new();
 
         [Fact]
         public async Task BaseTests()
         {
             var controller = new TestEntriesController();
 
-            var payload = new JokeRequestPackage()
+            var requestData = new JokeRequestPackage()
             {
                 EnvHeader = "null",
                 LogHeader = "null",
                 ApprovalHeader = "null",
                 RequestData = new JokeRequestContract()
             };
-            var responsePackage = await controller.GetRandomJoke(payload) as JsonResult;
+
+            var responsePackage = await controller.GetRandomJoke(requestData) as JsonResult;
             var jsonResponse = JsonConvert.SerializeObject(responsePackage);
-            assert.AssertSuccess(responsePackage.Value as JokeResponsePackage);
+            Assert.AssertSuccess(responsePackage.Value as JokeResponsePackage);
         }
 
     }
