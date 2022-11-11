9f234c9e746886ab0e2e1cbcec185946ab858dbd
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-11-11 18:07:06 +0200

    testing

---
+++ dotnet/Examples/TestEntries/TestEntries.Common/Services/ContractAssert.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestEntries.Common.Contracts;
+using Xunit;
+
+namespace TestEntries.Common.Services
+{
+    public class ContractAssert : Assert
+    {
+        public void AssertSuccess<T>(PackageResponse<T> responsePackage) where T: new()
+        {
+            True(responsePackage.ResponseHeader.Errors == null);
+            Equal(responsePackage.ResponseHeader.ResultStatus, true);
+        }
+    }
+}
---
+++ dotnet/Examples/TestEntries/TestEntries.UnitTests/Tests/GetRandomJokeTests.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestEntries.Api.Controllers;
+using TestEntries.Common.Contracts;
+using TestEntries.Common.Services;
+using TestEntries.Contracts.Contracts.Joke;
+
+namespace TestEntries.UnitTests.Tests
+{
+    public class GetRandomJokeTests
+    {
+        private static readonly ContractAssert assert = new();
+
+        [Fact]
+        public async Task BaseTests()
+        {
+            var controller = new TestEntriesController();
+
+            var payload = new JokeRequestPackage()
+            {
+                EnvHeader = "null",
+                LogHeader = "null",
+                ApprovalHeader = "null",
+                RequestData = new JokeRequestContract()
+            };
+            var responsePackage = await controller.GetRandomJoke(payload) as JsonResult;
+            var jsonResponse = JsonConvert.SerializeObject(responsePackage);
+            assert.AssertSuccess(responsePackage.Value as JokeResponsePackage);
+        }
+
+    }
+}
