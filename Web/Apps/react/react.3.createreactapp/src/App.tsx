146330ae364b02fd0f59c4a2ce3c71f3b958b24e
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-11-13 17:42:04 +0200

    Update App.tsx

--- Web/Apps/react/react.3.createreactapp/src/App.tsx
+++ Web/Apps/react/react.3.createreactapp/src/App.tsx
@@ -11,12 +11,34 @@ function MyButton() {
   return <button onClick={increment}>{count} clicks!</button>;
 }
 
+function DependentButton({ count, onClick }: any) {
+  return <button onClick={onClick}>{count} clicks!</button>;
+}
+
+function DependentButtons() {
+  const [count, countSet] = useState(0);
+
+  function increment(): void {
+    countSet(count + 1);
+  }
+
+  return (
+    <div>
+      <DependentButton count={count} onClick={increment} />
+      <DependentButton count={count} onClick={increment} />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Counter</h1>
+      <h2>Independent buttons</h2>
       <MyButton />
       <MyButton />
+      <h2>Dependent buttons</h2>
+      <DependentButtons />
     </div>
   );
 }
