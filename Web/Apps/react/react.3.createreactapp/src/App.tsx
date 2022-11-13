9ad4e13178c897f34f36ecebe0102aa7da59812c
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2022-11-13 17:01:52 +0200

    ts

--- Web/Apps/react/react.3.createreactapp/src/App.tsx
+++ Web/Apps/react/react.3.createreactapp/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+function MyButton() {
+  const [count, countSet] = useState(0);
+
+  function increment(): void {
+    countSet(count + 1);
+  }
+
+  return <button onClick={increment}>{count} clicks!</button>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <MyButton />
+      <MyButton />
     </div>
   );
 }
