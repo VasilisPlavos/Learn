ab67937f6273541c37192ccc880212e2c1f0e62e
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2023-02-11 19:28:16 +0200

    add codility test

---
+++ dotnet/Examples/coderbyte.com/Com.Codebyte.Solutions/Solutions/Codility/BinaryGap.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Com.Coderbyte.Solutions.Solutions.Codility;
+
+public class BinaryGap
+{
+	public static void Test()
+	{
+		var input = 1041;
+		var outputExpected = 5;
+		Run(input, outputExpected);
+
+		input = 15;
+		outputExpected = 0;
+		Run(input, outputExpected);
+
+		input = 32;
+		outputExpected = 0;
+		Run(input, outputExpected);
+	}
+
+	private static void Run(int input, int outputExpected)
+	{
+		Console.WriteLine($"INPUT: {string.Join(", ", input)}");
+		Console.WriteLine($"OUTPUT EXPECTED: {outputExpected}");
+
+		var output = Solution(input);
+		Console.WriteLine($"OUTPUT: {output}");
+
+		Console.WriteLine(output == outputExpected ? "CORRECT" : "NOT CORRECT");
+		System.Console.WriteLine("");
+	}
+
+	private static int Solution(int N)
+	{
+		var binary = Convert.ToString(N, 2);
+
+		while (binary.EndsWith("0")) binary = binary.Remove(binary.Length - 1, 1);
+		while (binary.StartsWith("0")) binary = binary.Remove(1);
+
+		var longest = 0;
+		var curCount = 0;
+		foreach (var c in binary)
+		{
+			if (c == '0')
+			{
+				if (curCount > 0) curCount++;
+				else curCount = 1;
+			}
+			else curCount = 0;
+			if (curCount > longest) longest = curCount;
+		}
+
+		return longest;
+	}
+}
--- dotnet/Examples/coderbyte.com/Com.Codebyte.Solutions/Solutions/Codility/Codility.cs
+++ dotnet/Examples/coderbyte.com/Com.Codebyte.Solutions/Solutions/Codility/Codility.cs
@@ -5,18 +5,20 @@ namespace Com.Coderbyte.Solutions.Solutions.Codility
     public class Codility
     {
         public static void Tests()
-        {
+		{
             TestHelper.Console.WriteLine.Start("Codility demo test");
             MissingInteger.Test();
 
             TestHelper.Console.WriteLine.Start("Codility FileRenamer");
             FileRenamer.Test();
 
-            //TestHelper.Console.WriteLine.Start("Codility Test1");
-            //Task1.Test();
+            BinaryGap.Test();
 
-            //TestHelper.Console.WriteLine.Start("Codility Test2");
-            //Task2.Test();
-        }
+			//TestHelper.Console.WriteLine.Start("Codility Test1");
+			//Task1.Test();
+
+			//TestHelper.Console.WriteLine.Start("Codility Test2");
+			//Task2.Test();
+		}
     }
 }
