9f7a43184ebe43f74b986363c5743f626229dda6
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2023-03-04 02:51:02 +0200

    finished 3.13

---
+++ dotnet/Courses/Learn_CSharp_Basics/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string numberString = "277";
+int numberInt = Convert.ToInt32(numberString);
+byte numberByte = (byte)numberInt;
+Console.WriteLine(numberByte);
