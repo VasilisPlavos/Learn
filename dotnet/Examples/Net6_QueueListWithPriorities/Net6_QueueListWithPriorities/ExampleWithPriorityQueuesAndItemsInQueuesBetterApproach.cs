7b7b04afc2b6d6dae39a0e5111ae0d5cce2ecd21
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2023-03-06 16:51:12 +0200

    Update ExampleWithPriorityQueuesAndItemsInQueuesBetterApproach.cs

--- dotnet/Examples/Net6_QueueListWithPriorities/Net6_QueueListWithPriorities/ExampleWithPriorityQueuesAndItemsInQueuesBetterApproach.cs
+++ dotnet/Examples/Net6_QueueListWithPriorities/Net6_QueueListWithPriorities/ExampleWithPriorityQueuesAndItemsInQueuesBetterApproach.cs
@@ -11,13 +11,14 @@ public static class ExampleWithPriorityQueuesAndItemsInQueuesBetterApproach
         Console.WriteLine("This solution is FIFO. 36 comes after 31");
 
         var cts = new CancellationTokenSource();
-        var queue = new PriorityQueue<QueueItem, (int, long)>();
-        queue.Enqueue(new QueueItem { Name = "31", PriorityTitle = "Normal" }, (3, Stopwatch.GetTimestamp()));
-        queue.Enqueue(new QueueItem { Name = "12", PriorityTitle = "Platinum" }, (1, Stopwatch.GetTimestamp()));
-        queue.Enqueue(new QueueItem { Name = "23", PriorityTitle = "Gold" }, (2, Stopwatch.GetTimestamp()));
-        queue.Enqueue(new QueueItem { Name = "24", PriorityTitle = "Gold" }, (2, Stopwatch.GetTimestamp()));
-        queue.Enqueue(new QueueItem { Name = "15", PriorityTitle = "Platinum" }, (1, Stopwatch.GetTimestamp()));
-        queue.Enqueue(new QueueItem { Name = "36", PriorityTitle = "Normal" }, (3, Stopwatch.GetTimestamp()));
+        var queue = new PriorityQueue<QueueItem, (int, DateTime)>();
+
+        queue.Enqueue(new QueueItem { Name = "31", PriorityTitle = "Normal" }, (3, DateTime.UtcNow));
+        queue.Enqueue(new QueueItem { Name = "12", PriorityTitle = "Platinum" }, (1, DateTime.UtcNow));
+        queue.Enqueue(new QueueItem { Name = "23", PriorityTitle = "Gold" }, (2, DateTime.UtcNow));
+        queue.Enqueue(new QueueItem { Name = "24", PriorityTitle = "Gold" }, (2, DateTime.UtcNow));
+        queue.Enqueue(new QueueItem { Name = "15", PriorityTitle = "Platinum" }, (1, DateTime.UtcNow));
+        queue.Enqueue(new QueueItem { Name = "36", PriorityTitle = "Normal" }, (3, DateTime.UtcNow));
 
         while (!cts.IsCancellationRequested)
         {
