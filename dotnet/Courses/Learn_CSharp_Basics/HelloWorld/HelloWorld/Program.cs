f73db31ffb869931a29c32dd2ba6e1253efa4348
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2023-03-15 13:37:25 +0200

    Update Program.cs

--- dotnet/Courses/Learn_CSharp_Basics/HelloWorld/HelloWorld/Program.cs
+++ dotnet/Courses/Learn_CSharp_Basics/HelloWorld/HelloWorld/Program.cs
@@ -1,7 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Text;
+
+
+
 Console.WriteLine("Hello, World!");
 
-string numberString = "277";
-int numberInt = Convert.ToInt32(numberString);
-byte numberByte = (byte)numberInt;
-Console.WriteLine(numberByte);
+// here: C:\Users\vplav\Bill\Downloads\Tor\Files\Courses\[FreeCourseLab.com] Udemy - C# Intermediate Classes, Interfaces and OOP\2. Classes\8. Indexers.mp4
+
+
+
+var builder = new StringBuilder();
+builder.Append('-', 10);
+Console.WriteLine(builder);
+
+var person = new Person("Jack");
+
+person.PhoneList.Add("1");
+person.PhoneList.Add("2");
+
+foreach (var i in person.PhoneList)
+{
+	Console.WriteLine(i);
+}
+
+Console.WriteLine(person.Name);
+
+public class Person
+{
+	public readonly string? Name;
+	public readonly List<string> PhoneList = new(){"7"};
+
+	public Person(string? name)
+	{
+		Name = name;
+	}
+}
