fba7d91ad58623cee4edf17aa973a43f1acb18a3
Vasilis <vplavos@gmail.com>
2023-06-30 21:48:40 +0200

    Add Auth.v3.SimpleJwtWithCustomPolicies

---
+++ dotnet/Examples/Auth multiple examples/Auth.v3.SimpleJwtWithCustomPolicies/PoC.Authentication.API/Services/IProjectsService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PoC.Authentication.API.Data;
+using PoC.Authentication.API.Entities;
+using System.Security.Cryptography;
+using PoC.Authentication.API.Helpers;
+
+namespace PoC.Authentication.API.Services;
+
+public interface IProjectsService
+{
+    Task<Project> CreateUserProjectAsync(HttpRequest request);
+    Task<List<Project>> GetUserProjectsAsync(HttpRequest request);
+}
+
+public class ProjectsService : IProjectsService
+{
+    private readonly ApplicationDbContext _db;
+    public ProjectsService(ApplicationDbContext db) { _db = db; }
+    public async Task<Project> CreateUserProjectAsync(HttpRequest request)
+    {
+        var userId = UtilHelper.GetUserId(request.Headers);
+        var project = new Project()
+        {
+            OwnerId = userId,
+            Title = $"Project {RandomNumberGenerator.GetInt32(1, 10000)}"
+        };
+
+        _db.Projects.Add(project);
+        await _db.SaveChangesAsync();
+        return project;
+    }
+    public async Task<List<Project>> GetUserProjectsAsync(HttpRequest request)
+    {
+        var userId = UtilHelper.GetUserId(request.Headers);
+        return await _db.Projects.Where(x => x.OwnerId == userId).ToListAsync();
+    }
+}
