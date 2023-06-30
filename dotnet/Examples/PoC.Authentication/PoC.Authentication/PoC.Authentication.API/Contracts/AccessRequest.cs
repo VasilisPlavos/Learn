d22546728729647f7c29caee80512947add9a5f9
Vasilis <vplavos@gmail.com>
2023-06-30 18:55:08 +0200

    remove old dir

--- dotnet/Examples/PoC.Authentication/PoC.Authentication/PoC.Authentication.API/Contracts/AccessRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PoC.Authentication.API.Contracts;
-
-public class AccessRequest
-{
-    public bool Anonymous { get; set; }
-    public string? Email { get; set; }
-    public string? SourceJwtToMove { get; set; }
-    public string? Password { get; set; }
-}
