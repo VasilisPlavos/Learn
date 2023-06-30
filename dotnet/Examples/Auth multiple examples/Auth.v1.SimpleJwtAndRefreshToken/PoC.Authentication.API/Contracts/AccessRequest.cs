13908c22e21b7165fbd648471f9992969a2c7cf3
Vasilis <vplavos@gmail.com>
2023-06-30 18:32:44 +0200

    simplest impl

---
+++ dotnet/Examples/Auth multiple examples/Auth.v1.SimpleJwtAndRefreshToken/PoC.Authentication.API/Contracts/AccessRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace PoC.Authentication.API.Contracts;
+
+public class AccessRequest
+{
+    public bool Anonymous { get; set; }
+    public string? Email { get; set; }
+    public string? SourceJwtToMove { get; set; }
+}
---
+++ dotnet/Examples/Auth multiple examples/Auth.v1.SimpleJwtAndRefreshToken/PoC.Authentication.API/Contracts/BearerSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace PoC.Authentication.API.Contracts
+{
+	public class BearerSettings
+	{
+		public string Secret { get; set; }
+		public List<string> ValidIssuers { get; set; }
+		public List<string> ValidAudiences { get; set; }
+	}
+}
