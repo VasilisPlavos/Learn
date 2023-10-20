2d56c425265504d13992cb7513b317f792183cc8
Vasilis <vplavos@gmail.com>
2023-10-20 20:38:01 +0200

    Update IInParallelService.cs

--- dotnet/Examples/Examples.23/Examples.23.InParallel/IInParallelService.cs
+++ dotnet/Examples/Examples.23/Examples.23.InParallel/IInParallelService.cs
@@ -13,15 +13,33 @@ public class InParallelService : IInParallelService
 {
     private static readonly HttpClient HttpClient = new();
     private const int TaskCount = 1000;
-    private const string RegExPattern = "value\":\"([^\"]+)\"";
 
     public async Task<bool> RunAsync()
     {
-        var parallelList = ParallelVersion();
-        var tasksList2 = await ForEachVersion2Async();
-        var parallelList2 = await ParallelVersionByFiratAsync();
+        // ParallelVersionWithOptions took 00:00:12.6815484
+        var start = DateTime.UtcNow;
         var parallelList3 = ParallelVersionWithOptions();
+        Console.WriteLine($"{nameof(ParallelVersionWithOptions)} took {(DateTime.UtcNow - start).TotalSeconds} sec");
+
+        // parallelList2 took 00:00:04.4084283
+        start = DateTime.UtcNow;
+        var parallelList2 = await ParallelVersionByFiratAsync();
+        Console.WriteLine($"{nameof(ParallelVersionByFiratAsync)} took {(DateTime.UtcNow - start).TotalSeconds} sec");
+
+        // parallelList took 00:00:05.6568826
+        start = DateTime.UtcNow;
+        var parallelList = ParallelVersion();
+        Console.WriteLine($"{nameof(ParallelVersion)} took {(DateTime.UtcNow - start).TotalSeconds} sec");
+
+        // tasksList2 took 00:00:05.4613256
+        start = DateTime.UtcNow;
+        var tasksList2 = await ForEachVersion2Async();
+        Console.WriteLine($"{nameof(ForEachVersion2Async)} took {(DateTime.UtcNow - start).TotalSeconds} sec");
+
+        // tasksList took 00:01:47.5823954
+        start = DateTime.UtcNow;
         var tasksList = await ForEachVersionAsync();
+        Console.WriteLine($"{nameof(ForEachVersionAsync)} took {(DateTime.UtcNow - start).TotalSeconds} sec");
         return true;
     }
 
@@ -34,9 +52,6 @@ public class InParallelService : IInParallelService
             .ToList();
 
         foreach (var task in tasksList) list.Add(await task());
-        list.Insert(0, $"Started: {DateTime.UtcNow}");
-        list.Add($"Ended: {DateTime.UtcNow}");
-
         return list;
     }
 
@@ -46,12 +61,7 @@ public class InParallelService : IInParallelService
             .Select(_ => GetJokeAsync(HttpClient));
 
         var results = await Task.WhenAll(tasksList);
-
-        var list = results.ToList();
-        list.Insert(0, $"Started: {DateTime.UtcNow}");
-        list.Add($"Ended: {DateTime.UtcNow}");
-
-        return list;
+        return results.ToList();
     }
 
     private static async Task<string> GetJokeAsync(HttpClient httpClient)
@@ -64,21 +74,19 @@ public class InParallelService : IInParallelService
     }
     private IEnumerable<string> ParallelVersion()
     {
-        var list = new ConcurrentBag<string> { $"Started: {DateTime.UtcNow}" };
+        var list = new ConcurrentBag<string>();
 
         var tasksList = Enumerable.Range(0, TaskCount)
             .Select(_ => new Func<string>(() => GetJokeAsync(HttpClient).GetAwaiter().GetResult()))
             .ToList();
 
         Parallel.For(0, tasksList.Count, i => list.Add(tasksList[i]()));
-        list.Add($"Ended: {DateTime.UtcNow}");
-
         return list;
     }
 
     private async Task<IReadOnlyCollection<string>> ParallelVersionByFiratAsync()
     {
-        var list = new ConcurrentBag<string> { $"Started: {DateTime.UtcNow}" };
+        var list = new ConcurrentBag<string>();
 
         var tasksList = Enumerable.Range(0, TaskCount)
             .Select(_ => GetJokeAsync(HttpClient))
@@ -91,8 +99,6 @@ public class InParallelService : IInParallelService
         };
 
         await Parallel.ForEachAsync(tasksList, parallelOptions: options, body: async (strTask, _) => { list.Add(await strTask); });
-        list.Add($"Ended: {DateTime.UtcNow}");
-
         return list;
     }
 
@@ -100,15 +106,13 @@ public class InParallelService : IInParallelService
     // Important -> Use IReadOnlyCollection and not List 
     private IReadOnlyCollection<string> ParallelVersionWithOptions()
     {
-        var list = new ConcurrentBag<string> { $"Started: {DateTime.UtcNow}" };
+        var list = new ConcurrentBag<string>();
 
         var tasksList = Enumerable.Range(0, TaskCount)
             .Select(_ => new Func<string>(() => GetJokeAsync(HttpClient).GetAwaiter().GetResult()))
             .ToList();
 
-        Parallel.For(0, tasksList.Count, new ParallelOptions { MaxDegreeOfParallelism = 4 }, i => list.Add(tasksList[i]()));
-        list.Add($"Ended: {DateTime.UtcNow}");
-
+        Parallel.For(0, tasksList.Count, new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, i => list.Add(tasksList[i]()));
         return list;
     }
 }
