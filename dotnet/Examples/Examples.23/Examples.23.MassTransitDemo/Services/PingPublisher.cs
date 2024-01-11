1c9d1772adc2eb3b16ee0131da13b55e906eddd3
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-01-12 00:09:29 +0100

    implementation

--- dotnet/Examples/Examples.23/Examples.23.MassTransitDemo/Services/PingPublisher.cs
+++ dotnet/Examples/Examples.23/Examples.23.MassTransitDemo/Services/PingPublisher.cs
@@ -1,12 +1,16 @@
-﻿namespace Examples._23.MassTransitDemo.Services;
+﻿using MassTransit;
+
+namespace Examples._23.MassTransitDemo.Services;
 
 public class PingPublisher : BackgroundService
 {
     private readonly ILogger<PingPublisher> _logger;
+    private readonly IBus _bus;
 
-    public PingPublisher(ILogger<PingPublisher> logger)
+    public PingPublisher(ILogger<PingPublisher> logger, IBus bus)
     {
         _logger = logger;
+        _bus = bus;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -18,7 +22,8 @@ public class PingPublisher : BackgroundService
             var keyPressed = Console.ReadKey(true);
             if (keyPressed.Key != ConsoleKey.Escape)
             {
-                _logger.LogInformation($"Pressed {keyPressed.Key.ToString()}");
+               // _logger.LogInformation($"Pressed {keyPressed.Key}");
+                await _bus.Publish(new Ping(keyPressed.Key.ToString()), stoppingToken);
             }
 
             await Task.Delay(200);
