cc270d59584531fcd3e750b987275149e6e9cb08
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-01-24 00:08:01 +0100

    Implement Console app that calls a processor

---
+++ dotnet/Examples/Examples.23/Examples.y24.ImageSharp.ConsoleApp/ConsoleAppService.cs
@@ -0,0 +1,26 @@
+﻿using Examples._23.ImageSharp.Services;
+using Microsoft.Extensions.Hosting;
+
+namespace Examples.y24.ImageSharp.ConsoleApp;
+
+public class ConsoleAppService : IHostedService
+{
+    private readonly IImageSharpService _imageSharpService;
+
+    public ConsoleAppService(IImageSharpService imageSharpService)
+    {
+        _imageSharpService = imageSharpService;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Cleaner is running...");
+        await _imageSharpService.RunAsync();
+        Environment.Exit(0);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ dotnet/Examples/Examples.23/Examples.y24.ImageSharp.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Examples._23.ImageSharp.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Examples.y24.ImageSharp.ConsoleApp;
+
+static class Program
+{
+    static async Task Main(string[] args)
+    {
+        await CreateHostBuilder(args).RunConsoleAsync();
+    }
+
+    static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                config.AddEnvironmentVariables();
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                var connectionStringExample = hostContext.Configuration.GetConnectionString("connection");
+                Console.WriteLine($"{connectionStringExample}");
+                
+                services.AddScoped<IImageSharpService, ImageSharpService>();
+
+                services.AddHostedService<ConsoleAppService>();
+            });
+}
