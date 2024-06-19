a55f82c5b8a660d0f6f9c987b0e5324c6322b1e8
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-06-19 20:49:51 +0200

    c

---
+++ dotnet/Examples/Examples.y/Examples.y24.DataStructure/Helpers/XmlHelper.cs
@@ -0,0 +1,103 @@
+﻿using Examples.y24.Common.Dtos.Joke;
+using System.Xml.Linq;
+
+namespace Examples.y24.DataStructure.Helpers;
+
+public class XmlHelper
+{
+    public static async Task ExamplesAsync()
+    {
+        Example1();
+    }
+
+    private static void Example1()
+    {
+        string xml1 = @"<root><child1>text1</child1><child2 attr1='value1'>text2</child2></root>";
+        string xml2 = @"<root><child1>text1</child1><child2 attr1='value1'>text2</child2></root>";
+
+        var differences = CompareXml(xml1, xml2);
+    }
+
+
+    private static void Example2()
+    {
+        var doc1 = "<example>\r\n  <person>\r\n    <firstName/>\r\n\r\n    <tel/>\r\n  </person>\r\n</example>";
+        var doc2 = "<example>\r\n  <person>\r\n    <firstName/>\r\n    <lastName/>\r\n\r\n  </person>\r\n</example>";
+    }
+
+    private static void Example3()
+    {
+        string xml1 = @"<root><child1>text1</child1><child2 attr1='value1'>text2</child2></root>";
+        string xml2 = @"<root><child1>text1</child1><child2 attr2='value2'>text3</child2></root>";
+
+        var differences = CompareXml(xml1, xml2);
+    }
+
+    public static List<string> CompareXml(string xml1, string xml2)
+    {
+        XDocument doc1 = XDocument.Parse(xml1);
+        XDocument doc2 = XDocument.Parse(xml2);
+
+        return CompareNodes(doc1.Root, doc2.Root);
+    }
+
+    private static List<string> CompareNodes(XElement node1, XElement node2, bool compareNodeNames = true, bool compareNodeValues = true)
+    {
+        var differences = new List<string>();
+
+        if (compareNodeNames)
+        {
+            if (node1.Name != node2.Name)
+            {
+                differences.Add($"Tags differ: {node1.Name} vs {node2.Name}");
+            }
+        }
+
+        if (compareNodeValues)
+        {
+            if (node1.Value.Trim() != node2.Value.Trim())
+            {
+                differences.Add($"Texts differ: {node1.Value} vs {node2.Value}");
+            }
+        }
+
+        // Compare attributes
+        var attrs1 = node1.Attributes().ToDictionary(a => a.Name, a => a.Value);
+        var attrs2 = node2.Attributes().ToDictionary(a => a.Name, a => a.Value);
+
+        foreach (var attr in attrs1)
+        {
+            if (!attrs2.ContainsKey(attr.Key) || attrs2[attr.Key] != attr.Value)
+            {
+                differences.Add($"Attributes differ: {attr.Key} - {attr.Value} vs {attrs2.GetValueOrDefault(attr.Key)}");
+            }
+        }
+
+        // Compare child nodes recursively
+        var childNodes1 = node1.Nodes();
+        var childNodes2 = node2.Nodes();
+
+        for (int i = 0; i < childNodes1.Count(); i++)
+        {
+            if (childNodes2.Count() <= i)
+            {
+                //differences.Add($"Missing child node: {childNodes1.ElementAt(i).Name}");
+                differences.Add($"Missing child node: {childNodes1.ElementAt(i)}");
+                continue;
+            }
+
+            differences.AddRange(CompareNodes(childNodes1.ElementAt(i) as XElement, childNodes2.ElementAt(i) as XElement));
+        }
+
+        // Check for extra nodes in second XML
+        for (int i = childNodes2.Count(); i > childNodes1.Count(); i--)
+        {
+            //differences.Add($"Extra child node in second XML: {childNodes2.ElementAt(i).Name}");
+            differences.Add($"Extra child node in second XML: {childNodes2.ElementAt(i)}");
+        }
+
+        return differences;
+    }
+
+
+}
--- dotnet/Examples/Examples.y/Examples.y24.DataStructure/Program.cs
+++ dotnet/Examples/Examples.y/Examples.y24.DataStructure/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Examples.y24.DataStructure.Helpers;
 
+await XmlHelper.ExamplesAsync();
 DictionariesHelper.Examples();
 
 
