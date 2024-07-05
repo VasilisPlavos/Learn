5fd5cd8137609c943f961d82b7ee108f282d3b0b
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-07-05 20:46:43 +0200

    del

--- dotnet/Examples/Examples.y/Examples.y24.BackgroundTasks.WithWorkerAndTaskList/Consts.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections.Concurrent;
-
-namespace Examples.y24.BackgroundTasks.WithWorkerAndTaskList;
-
-public class Consts
-{
-    public static ConcurrentBag<Task> TaskList { get; set; } = new();
-}
