dd73ae488e577932ce8c7b45ba71e192d5918089
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-07-06 16:04:06 +0200

    Update UnitTest1.cs

--- dotnet/NuGets/nugetcheck/BrainSharp.NugetCheck.Tests/UnitTest1.cs
+++ dotnet/NuGets/nugetcheck/BrainSharp.NugetCheck.Tests/UnitTest1.cs
@@ -12,9 +12,9 @@ namespace BrainSharp.NugetCheck.Tests
         [Test]
         [TestCase("sixlabors.imagesharp", "3.1.3", true)]
         [TestCase("sixlabors.imagesharp", "3.1.4", false)]
-        //[TestCase("Newtonsoft.Json", "12.0.3", true)]
+        [TestCase("Newtonsoft.Json", "12.0.3", true)]
         //[TestCase("Newtonsoft.Json", "1.5.1", true)]
-        //[TestCase("Newtonsoft.Json", "13.0.3", false)]
+        [TestCase("Newtonsoft.Json", "13.0.3", false)]
         // [TestCase("dotnet-tool-outdated", "0.1.0", true)]
         // Methodname_Condition_Expectation
         public async Task IsVulnerable_StringContents_True(string packageName, string packageVersion, bool expected)
