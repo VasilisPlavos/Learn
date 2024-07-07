de99cc9683797dae1724329ed11e93cfb391bc7e
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-07-07 02:59:26 +0200

    Update LocalStorageService.cs

--- dotnet/NuGets/nugetcheck/BrainSharp.NugetCheck/Services/LocalStorageService.cs
+++ dotnet/NuGets/nugetcheck/BrainSharp.NugetCheck/Services/LocalStorageService.cs
@@ -6,7 +6,42 @@ namespace BrainSharp.NugetCheck.Services;
 public class LocalStorageService
 {
     private static readonly string StoragePath = Path.Combine(AppContext.BaseDirectory, "storage");
+    
+    public static async Task<NugetPackage?> GetNugetPackageAsync(string packageName)
+    {
+        var filePath = Path.Combine(StoragePath, packageName, "index.json");
+        try
+        {
+            using var sr = new StreamReader(filePath);
+            var json = await sr.ReadToEndAsync();
+            var nugetPackage = JsonConvert.DeserializeObject<NugetPackage>(json);
+            return nugetPackage;
+        }
+        catch
+        {
+            // ignored
+        }
 
+        return null;
+    }
+
+    public static async Task<NugetPackageVersionInfo?> GetNugetPackageVersionInfoAsync(string packageName, string packageVersion)
+    {
+        var filePath = Path.Combine(StoragePath, packageName, $"{packageVersion}.json");
+        try
+        {
+            using var sr = new StreamReader(filePath);
+            var json = await sr.ReadToEndAsync();
+            var nugetPackageVersionInfo = JsonConvert.DeserializeObject<NugetPackageVersionInfo>(json);
+            return nugetPackageVersionInfo;
+        }
+        catch
+        {
+            // ignored
+        }
+
+        return null;
+    }
 
     public static async Task SaveNugetPackageAsync(NugetPackage nugetPackage)
     {
@@ -42,41 +77,5 @@ public class LocalStorageService
         {
             Console.WriteLine(e);
         }
-    }
-
-    public static async Task<NugetPackage?> GetNugetPackageAsync(string packageName)
-    {
-        var filePath = Path.Combine(StoragePath, packageName, "index.json");
-        try
-        {
-            using var sr = new StreamReader(filePath);
-            var json = await sr.ReadToEndAsync();
-            var nugetPackage = JsonConvert.DeserializeObject<NugetPackage>(json);
-            return nugetPackage;
-        }
-        catch
-        {
-            // ignored
-        }
-
-        return null;
-    }
-
-    public static async Task<NugetPackageVersionInfo?> GetNugetPackageVersionInfoAsync(string packageName, string packageVersion)
-    {
-        var filePath = Path.Combine(StoragePath, packageName, $"{packageVersion}.json");
-        try
-        {
-            using var sr = new StreamReader(filePath);
-            var json = await sr.ReadToEndAsync();
-            var nugetPackageVersionInfo = JsonConvert.DeserializeObject<NugetPackageVersionInfo>(json);
-            return nugetPackageVersionInfo;
-        }
-        catch
-        {
-            // ignored
-        }
-
-        return null;
     }
 }
