47c266224e7ddd02db1d1a4caf84c501a20abd4c
Vasileios Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-07-08 17:40:35 +0200

    sr

--- dotnet/Courses/Learn_SignalR/SignalRSample-byInstructor/SignalRSample/Program.cs
+++ dotnet/Courses/Learn_SignalR/SignalRSample-byInstructor/SignalRSample/Program.cs
@@ -6,9 +6,12 @@ using SignalRSample.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options
+        .UseInMemoryDatabase("SignalRSample")
+        //.UseSqlServer(connectionString)
+    );
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
