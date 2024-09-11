8d3cd35e44b6528f24a7774df10ace72844b8cb2
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-09-11 02:15:35 +0200

    clean

--- Web/Others/Others/ContactRepositoryExample.js
+++ Web/Others/Others/ContactRepositoryExample.js
@@ -1,27 +1,23 @@
-var Module = (function (){
-    return {
-
-        Method: function  (a) {
-            console.log(a);
-        }
-
-    }})();
+var Module = (function () {
+  return {
+    Method: function (a) {
+      console.log(a);
+    },
+  };
+})();
 
 Module.Method(6);
 
+var _contactRepo = (function () {
+  return {
+    GetContactById: function (id) {
+      if (id != 1) {
+        return "NotFound";
+      }
+      return "Vasilis";
+    },
+  };
+})();
 
-var _contactRepo = (function(){
-    return {
-
-
-        GetContactById: function(id)
-        {
-            if (id != 1) {return 'NotFound'}
-            return 'Vasilis'
-        }
-    }
-})()
-
-
-var name = _contactRepo.GetContactById(1);
-console.log(name);
+var contact = _contactRepo.GetContactById(1);
+console.log(contact);
--- Web/Others/mathesis_web/javascript/t3.set-default-value.js
+++
@@ -1,7 +0,0 @@
-function myF ( myValue = 1 ){
-    var x = myValue + 1;
-    return x;
-}
-
-console.log(myF());
-console.log(myF(5));
