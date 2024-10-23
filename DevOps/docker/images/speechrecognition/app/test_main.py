120414e440719b7ba2dcf1d737fb3b218ea0bab2
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-10-24 01:04:17 +0200

    Update test_main.py

--- DevOps/docker/images/speechrecognition/app/test_main.py
+++ DevOps/docker/images/speechrecognition/app/test_main.py
@@ -7,23 +7,31 @@ class TestGetYoutubeId(unittest.TestCase):
         url = 'http://youtu.be/SA2iWivDJiE'
         self.assertEqual(get_youtube_id(url), 'SA2iWivDJiE')
 
-    def test_invalid_url(self):
-        url = 'invalid-url'
-        self.assertEqual(get_youtube_id(url), '')
+    def test_invalid_urls(self):
+
+        invalid_urls = [
+            'invalid-url',
+            '',
+            None
+            ]
+        
+        for url in invalid_urls:
+            self.assertEqual(get_youtube_id(url), '')
 
     def test_different_types_of_urls(self):
-            #Examples
-        url1='http://youtu.be/SA2iWivDJiE'
-        url2='http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu'
-        url3='http://www.youtube.com/embed/SA2iWivDJiE'
-        url4='http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US'
-        url5='https://www.youtube.com/watch?v=rTHlyTphWP0&index=6&list=PLjeDyYvG6-40qawYNR4juzvSOg-ezZ2a6'
-        url6='youtube.com/watch?v=_lOT2p_FCvA'
-        url7='youtu.be/watch?v=_lOT2p_FCvA'
-        url8='https://www.youtube.com/watch?time_continue=9&v=n0g-Y0oo5Qs&feature=emb_logo'
-        urls=[url1,url2,url3,url4,url5,url6,url7,url8]
 
-        for url in urls:
+        valid_urls = [
+            'http://youtu.be/SA2iWivDJiE',
+            'http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu',
+            'http://www.youtube.com/embed/SA2iWivDJiE',
+            'http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US',
+            'https://www.youtube.com/watch?v=rTHlyTphWP0&index=6&list=PLjeDyYvG6-40qawYNR4juzvSOg-ezZ2a6',
+            'youtube.com/watch?v=_lOT2p_FCvA',
+            'youtu.be/watch?v=_lOT2p_FCvA',
+            'https://www.youtube.com/watch?time_continue=9&v=n0g-Y0oo5Qs&feature=emb_logo'
+            ]
+
+        for url in valid_urls:
             id = get_youtube_id(url)
             self.assertEqual(len(id), 11)
 
