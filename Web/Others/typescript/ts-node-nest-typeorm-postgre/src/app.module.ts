1c78e787b99d7859abc151895aa90e8814b39263
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-11-08 02:23:21 +0100

    remove prettier added pg

--- Web/Others/typescript/ts-node-nest-typeorm-postgre/src/app.module.ts
+++ Web/Others/typescript/ts-node-nest-typeorm-postgre/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 6070,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'TEST_SM',
+      entities: [],
+      synchronize: true,
+      logging: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
