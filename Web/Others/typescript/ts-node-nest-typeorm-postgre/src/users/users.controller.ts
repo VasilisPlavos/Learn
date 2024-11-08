28d41410ff72daf5b925b78f7ca544bfbbbc0670
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-11-08 12:53:37 +0100

    update

--- Web/Others/typescript/ts-node-nest-typeorm-postgre/src/users/users.controller.ts
+++ Web/Others/typescript/ts-node-nest-typeorm-postgre/src/users/users.controller.ts
@@ -1,18 +1,27 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Patch, Post, Req } from '@nestjs/common';
 import { UserDto } from "./dtos/user.dto";
 import { UsersService } from "./users.service";
+import { ValidationPipe } from "src/utils/pipes/validation.pipe";
 
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
     @Post()
-    create(@Body() userDto: UserDto) {
-        return this.usersService.save(userDto);
+    async create(
+        @Body(new ValidationPipe()) userDto: UserDto, 
+        @Req() request: Request,
+        // @Res({ passthrough: true }) response: Response,
+        // @Next({ passthrough: true }) next: NextFunction
+) {
+        console.log(request);
+        const res = await this.usersService.save(userDto);
+        return res;
     }
 
     @Get()
     findAll() {
+        // throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
         return this.usersService.findAll();
     }
 
