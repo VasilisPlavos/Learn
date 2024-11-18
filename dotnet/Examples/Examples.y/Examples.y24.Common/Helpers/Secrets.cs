5d90488ebecd9f2dc13e01dc21a7d6edeba9c5db
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-11-18 21:56:41 +0100

    secrets quick impl

---
+++ dotnet/Examples/Examples.y/Examples.y24.Common/Helpers/Secrets.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Examples.y24.Common.Helpers
+{
+    public static class Secrets
+    {
+        private static readonly string SecretFilePath = Path.Combine(AppContext.BaseDirectory, "Secrets.json");
+        public static string Get(string keyValue)
+        {
+            try
+            {
+                var jsonContent = File.ReadAllText(SecretFilePath);
+                var result = JsonNode.Parse(jsonContent)?[keyValue]?.ToString();
+                return result ?? "";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return "";
+        }
+    }
+}
