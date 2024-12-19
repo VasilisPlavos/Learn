462c8801092f6dd1dbfc259994602ca87d0a2d0f
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2024-12-19 04:42:56 +0100

    cqrs example

---
+++ dotnet/Examples/Y25.CqrsExample/Y25.CqrsExample.Infrastructure/Databases/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Y25.CqrsExample.Domain.Entities;
+
+namespace Y25.CqrsExample.Infrastructure.Databases;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Contact> Contacts { get; set; }
+}
---
+++ dotnet/Examples/Y25.CqrsExample/Y25.CqrsExample.Processors/Dtos/ContactsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Y25.CqrsExample.Processors.Dtos;
+
+public class ContactDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
