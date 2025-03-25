a97c9dfe7bdec1a6c82a051c59ac16f069108148
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-03-25 19:58:09 +0100

    Update _layout.tsx

--- react-native/app/app/_layout.tsx
+++ react-native/app/app/_layout.tsx
@@ -1,25 +1,43 @@
 import { Stack } from "expo-router";
+import { StyleSheet } from "react-native";
 const RootLayout = () => {
-  return <Stack
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: "#f4511e",
-      },
-      headerTintColor: "#fff",
-      headerTitleStyle: {
-        fontSize: 20,
-        fontWeight: "bold",
-      },
-      contentStyle: {
-        paddingHorizontal: 10,
-        paddingTop: 10,
-        backgroundColor: "#fff",
-      },
-    }}>
-    <Stack.Screen name="index" options={{ title: "Booya!" }} />
-    <Stack.Screen name="about" options={{ title: "Abouht" }} />
-    <Stack.Screen name="notes" options={{ headerTitle: "notes@" }} />
-  </Stack>
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#ff8c00',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: 'bold',
+        },
+        contentStyle: {
+          paddingHorizontal: 10,
+          paddingTop: 10,
+          backgroundColor: '#fff',
+        },
+      }}
+    >
+      <Stack.Screen name='index' options={{ title: 'Home' }} />
+      <Stack.Screen name='notes' options={{ headerTitle: 'Notes' }} />
+    </Stack>
+  )
 }
+
+const styles = StyleSheet.create({
+  logoutButton: {
+    marginRight: 15,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    backgroundColor: '#ff3b30',
+    borderRadius: 8,
+  },
+  logoutText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default RootLayout;
