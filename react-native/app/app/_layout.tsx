d6a5426d7fa3674b6224b79d9a197ab3037efe17
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-03-25 19:56:17 +0100

    Update _layout.tsx

--- react-native/app/app/_layout.tsx
+++ react-native/app/app/_layout.tsx
@@ -1,5 +1,25 @@
 import { Stack } from "expo-router";
-
-export default function RootLayout() {
-  return <Stack />;
+const RootLayout = () => {
+  return <Stack
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: "#f4511e",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        fontSize: 20,
+        fontWeight: "bold",
+      },
+      contentStyle: {
+        paddingHorizontal: 10,
+        paddingTop: 10,
+        backgroundColor: "#fff",
+      },
+    }}>
+    <Stack.Screen name="index" options={{ title: "Booya!" }} />
+    <Stack.Screen name="about" options={{ title: "Abouht" }} />
+    <Stack.Screen name="notes" options={{ headerTitle: "notes@" }} />
+  </Stack>
 }
+
+export default RootLayout;
