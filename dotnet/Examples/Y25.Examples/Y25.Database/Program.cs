63f65f3cfc739d5678776ae927b062bbff7ae6ea
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-04-07 15:11:29 +0200

    yXX examples solution -> add EF packages

--- dotnet/Examples/Y25.Examples/Y25.Database/Program.cs
+++ dotnet/Examples/Y25.Examples/Y25.Database/Program.cs
@@ -2,6 +2,10 @@ using Y25.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Use the Package Manager Console to make database connection and push entities to database.
+// On Package Manager Console select `Y25.Database` as Default project
+// Right click on `Y25.Database` project and Set as startup project`
 // add-migration db1
 // update-database
 builder.Services.AddDbServices(builder.Configuration);
