6ebc557b099f8a2e8447562323edcc8025d8d65e
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-04-08 23:20:14 +0200

    Update FlatfoxService.cs

--- dotnet/Examples/Y25.Examples/Y25.ManyProcessors/Processors/FlatfoxService.cs
+++ dotnet/Examples/Y25.Examples/Y25.ManyProcessors/Processors/FlatfoxService.cs
@@ -14,24 +14,29 @@ public class FlatfoxService
     public async Task<List<PinDto>?> GetPinsAsync(GetPinsRequestDto request)
     {
         var pinsResponse = await _client.GetAsync($"https://flatfox.ch/api/v1/pin/?east={request.East}&north={request.North}&south={request.South}&west={request.West}&max_count={request.MaxCount}");
-        if (pinsResponse.IsSuccessStatusCode) return await pinsResponse.Content.ReadFromJsonAsync<List<PinDto>>();
-
-        var responseString = await pinsResponse.Content.ReadAsStringAsync();
-        Console.WriteLine(responseString);
-        throw new Exception($"Error while getting pins from Flatfox API. Status code: {pinsResponse.StatusCode}. Response: {responseString}.");
+        try
+        {
+            return await pinsResponse.Content.ReadFromJsonAsync<List<PinDto>>();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            var responseString = await pinsResponse.Content.ReadAsStringAsync();
+            Console.WriteLine(responseString);
+            throw new Exception($"Error while getting pins from Flatfox API. Status code: {pinsResponse.StatusCode}. Response: {responseString}.");
+        }
     }
 
     // This method is used to get listings by ids. It is used in the FlatfoxService class.
     // The limit of ids is around 450
-    public async Task<List<ListingResponseDto>> GetListingsAsync(List<int> ids)
+    public async Task<List<ListingResponseDto>?> GetListingsAsync(List<int> ids)
     {
         var idsString = string.Join("&pk=", ids);
         var url = $"https://flatfox.ch/api/v1/public-listing/?expand=cover_image&include=is_liked&include=is_disliked&include=is_subscribed&limit=0&pk={idsString}";
         var response = await _client.GetAsync(url);
         try
         {
-            var listings = await response.Content.ReadFromJsonAsync<List<ListingResponseDto>>();
-            return listings!;
+            return await response.Content.ReadFromJsonAsync<List<ListingResponseDto>>();
         }
         catch (Exception e)
         {
