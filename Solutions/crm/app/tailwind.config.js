647013ca0d6faa91a670d09939c50f62940f6bf6
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-06-09 01:36:40 +0200

    cmt

--- Solutions/crm/app/tailwind.config.js
+++ Solutions/crm/app/tailwind.config.js
@@ -1,6 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  // NativeWind: Add the paths to all of the component files
+  // https://www.nativewind.dev/docs/getting-started/installation#2-setup-tailwind-css
+  content: [
+    './app/**/*.{js,ts,tsx}',
+    './assets/**/*.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './constants/**/*.{js,ts,tsx}',
+    './hooks/**/*.{js,ts,tsx}'
+  ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
