3a12692c116afdfd59bcdb26ecc63fbc9c10904f
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-07-01 00:25:15 +0200

    Update index.ts

--- DevOps/docker/images/react-node/backend/src/index.ts
+++ DevOps/docker/images/react-node/backend/src/index.ts
@@ -2,9 +2,14 @@ import express, { NextFunction, Request, Response } from 'express';
 
 const app = express();
 app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.post('/api/hello', (req: Request, res: Response, next: NextFunction) => {
-  const name  = req.query.name ?? "unknown";
+  const name = req.query.name ?? "unknown";
   res.json({ hello: name });
 });
 
@@ -12,5 +17,5 @@ app.get('/api/hello', (req: Request, res: Response, next: NextFunction) => {
   res.json({ status: 'api is running' });
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
