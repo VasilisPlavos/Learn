dd98bc5cfc7003c3201472a28f525bb939cc05d9
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-08-14 16:20:15 +0200

    codebase

---
+++ web/nodejs/y25.node-ts-express-w-tests/README.md
@@ -0,0 +1,4 @@
+run with this:
+option 1: npx nodemon ./main.ts
+option 2: npx tsc main.ts && node main.js
+option 3: npm install && npm start
---
+++ web/nodejs/y25.node-ts-express-w-tests/src/index.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import express, { Request, Response } from 'express';
+import cors from "cors";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/v1/:id', async (req: Request, res: Response) => {
+
+    const { id } = req.params;
+    if (id === '2') return res.status(200).json({ result: 2 });
+
+    try {
+        const response = await axios.get('https://api.chucknorris.io/jokes/random');
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({ error: 'Error fetching joke from external API' });
+    }
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`server running. Check http://localhost:${PORT}/api/v1/2`);
+});
