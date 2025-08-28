f52a1eb44858a9c7e02b91eb5ca7556c5f6576b1
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-08-28 16:26:01 +0200

    cmt

--- web/nodejs/y25.node-ts-express-sequelize-w-tests/README.md
+++ web/nodejs/y25.node-ts-express-sequelize-w-tests/README.md
@@ -1,3 +1,13 @@
+### How to run:
+
+1. Go to src\data\databases.ts and uncomment `sequelize.sync` to do the migrations. Important: Don't do this on production
+2. npm run dev
+
+### How to add new entity
+
+1. Create the entity in entities
+2. add the entity in databases.ts in `createDatabase()`
+
 run with this:
 option 1: npx nodemon ./main.ts
 option 2: npx tsc main.ts && node main.js
--- web/nodejs/y25.node-ts-express-sequelize-w-tests/src/middlewares/errors.ts
+++ web/nodejs/y25.node-ts-express-sequelize-w-tests/src/middlewares/errors.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json({ error: 'An internal server error occurred' });
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   console.error(err);
+  res.status(err.statusCode || 500).json({ error: err.message || "An internal server error occurred" });
 };
