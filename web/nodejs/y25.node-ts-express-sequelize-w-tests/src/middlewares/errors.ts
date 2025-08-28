315e2dd1f9478b18a47c78cd0edb0ad06a754412
Vasilis Plavos <22474769+VasilisPlavos@users.noreply.github.com>
2025-08-28 16:02:01 +0200

    Update errors.ts

--- web/nodejs/y25.node-ts-express-sequelize-w-tests/src/middlewares/errors.ts
+++ web/nodejs/y25.node-ts-express-sequelize-w-tests/src/middlewares/errors.ts
@@ -2,5 +2,5 @@ import { NextFunction, Request, Response } from "express";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ error: 'An internal server error occurred' });
-  console.error(err.message);
+  console.error(err);
 };
